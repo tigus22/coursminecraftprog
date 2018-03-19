c47684a78613de84e2953187623c1a07a4bcdc94
FireFighter1017 <pascalbellerose@gmail.com>
2018-03-19 19:09:17 -0400

    scripts

---
+++ script/casentlanature.js
@@ -0,0 +1,37 @@
+// Strings can be used to display or store text values
+//console.log("hello"); 
+/* 
+    Strings can be any word of sentence as long as it is between
+    single-quote or double-quotes (apostrophes ou guillemets)
+*/
+//console.log('Ça sent la nature'); 
+//console.log("Ça sent d'la schnoute");
+
+// Numeric values don't need any delimiters
+//console.log(45);
+
+x = "toto";
+
+console.log(x);
+
+        // How to declare variables
+console.log(x);
+x = 5; 
+y = 6;      // How to assign values
+z = x + y;         // How to compute values
+
+console.log(z);
+
+x = "5"; 
+y = 6;      // How to assign values
+z = x + y;         // How to compute values
+
+console.log(z);
+
+
+
+
+
+
+
+
